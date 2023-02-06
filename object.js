59f130b66188babcc5f619b003f7340c3a3ba033
sajim-khan <shossain1427@gmail.com>
2023-02-06 15:24:38 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,22 @@
+var student = {
+    id : 10,
+    name : "Sajim",
+    class : 10,
+    marks : 80
+}
+
+console.log('student property:',student);
+
+var myComputer = {
+    brand : 'Hp',
+    price : 50000,
+    color : 'white',
+    processor : 'i5'
+}
+
+console.log('myComputer property:',myComputer);
+console.log(myComputer.brand);
+//update properties value
+myComputer.brand = 'Lenevo';  
+console.log('myComputer property:',myComputer);
+console.log(myComputer.brand);
