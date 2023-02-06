63bec10be377696fb79e67b45c40662ded1d1511
sajim-khan <shossain1427@gmail.com>
2023-02-06 21:00:47 +0600

    added switch

---
+++ switch.js
@@ -0,0 +1,36 @@
+// var color = 'green';
+// if (color === 'yellow'){
+//     console.log("This vegetable is yellow");
+// } else if (color === 'blue'){
+//     console.log("This vegetable is blue");
+// } else if (color === 'black'){
+//     console.log("This vegetable is black");
+// } else if (color === 'white'){
+//     console.log("This vegetable is white");
+// } else if (color === 'green'){
+//     console.log("This vegetable is green");
+// } else{
+//     console.log("This vegetable is red");
+// }
+
+
+//switch
+switch(color){
+    case 'green': 
+        console.log("This vegetable is green")
+        break;
+    case 'yellow':
+        console.log("This vegetable is yellow");
+        break;
+    case 'blue':
+        console.log("This vegetable is blue")
+        break;
+    case 'white':
+        console.log("This vegetable is white");
+        break;
+    case 'black':
+        console.log("This vegetable is black");
+        break;
+    default:
+        console.log("This vegetable is red");
+}
